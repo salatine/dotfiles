1d78bfeb38374488f754f929578dfce73dac834d
sawurora <sawurora@gmail.com>
2023-03-05 14:39:57 -0300

    lvim transparency

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -39,6 +39,7 @@ lvim.lsp.diagnostics.severity_sort = false
 lvim.log.level = "warn"
 lvim.format_on_save.enabled = false
 lvim.colorscheme = "lunar"
+lvim.transparent_window = true
 -- to disable icons and use a minimalist setup, uncomment the following
 -- lvim.use_icons = false
 
