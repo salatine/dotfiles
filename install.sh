02744fa46cc8a02b4faa604a0a33a5c9ea5e4852
salatine <95940523+salatinee@users.noreply.github.com>
2023-04-24 19:11:03 +0000

    add ignore files and share folders

--- install.sh
+++ install.sh
@@ -1,39 +1,70 @@
 scriptDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+shareFolders=( ".fonts" ".icons" ".themes" )
 home="$HOME"
+ignoreFiles=( "install.sh" "readme.md" ".git" )
 
-# for each file in the dotfiles directory, check if it exists in the home directory, if it does then ask if it should be overwritten, if it doesn't then copy it
-# create home/.config if it doesn't exist
-# for every folder in dotfiles/.config/, check if it exists in the home/.config/ directory, if it does then ask if it should be overwritten, if it doesn't then copy it
+main() {
+    addRootFiles
+    checkConfig
+    addConfigFiles
+    addShareFolders
+}
 
-for file in $(find "$scriptDir" -maxdepth 1 -type f); do
-    if [ -f "$home/$(basename $file)" ]; then
-        echo "$home/$(basename $file) already exists, overwrite? (y/n)"
-        read overwrite
-        if [ "$overwrite" == "y" ]; then
+addRootFiles() {
+    for file in $(find "$scriptDir" -maxdepth 1 -type f); do
+        for ignore in "${ignoreFiles[@]}"; do
+            if [ "$file" == "$scriptDir/$ignore" ]; then
+                continue 2
+            fi
+        done
+        if [ -f "$home/$(basename $file)" ]; then
+            echo "$home/$(basename $file) already exists, overwrite? (y/n)"
+            read overwrite
+            if [ "$overwrite" == "y" ]; then
+                cp $file $home
+                echo "Overwrote $home/$(basename $file)"
+            fi
+        else
             cp $file $home
-            echo "Overwrote $home/$(basename $file)"
+            echo "Copied $file to $home"
         fi
-    else
-        cp $file $home
-        echo "Copied $file to $home"
+    done
+}
+
+checkConfig() {
+    if [ ! -d "$home/.config" ]; then
+        mkdir $home/.config
+        echo "Created $home/.config"
     fi
-done
+}
 
-if [ ! -d "$home/.config" ]; then
-    mkdir $home/.config
-    echo "Created $home/.config"
-fi
-
-for folder in $(find "$scriptDir/.config" -maxdepth 1 -type d | grep ".config/"); do
-    if [ -d "$home/.config/$(basename $folder)" ]; then
-        echo "$home/.config/$(basename $folder) already exists, overwrite? (y/n)"
-        read overwrite
-        if [ "$overwrite" == "y" ]; then
+addConfigFiles() {
+    for folder in $(find "$scriptDir/.config" -maxdepth 1 -type d | grep ".config/"); do
+        if [ -d "$home/.config/$(basename $folder)" ]; then
+            echo "$home/.config/$(basename $folder) already exists, overwrite? (y/n)"
+            read overwrite
+            if [ "$overwrite" == "y" ]; then
+                cp -r $folder $home/.config
+                echo "Overwrote $home/.config/$(basename $folder)"
+            fi
+        else
             cp -r $folder $home/.config
-            echo "Overwrote $home/.config/$(basename $folder)"
+            echo "Copied $folder to $home/.config"
         fi
-    else
-        cp -r $folder $home/.config
-        echo "Copied $folder to $home/.config"
-    fi
-done
+    done
+}
+
+addShareFolders() {
+    for folder in "${shareFolders[@]}"; do
+        shareFolder=${folder:1}
+        if [ ! -d "$home/.local/share/$shareFolder" ]; then
+            
+            mkdir -p $home/.local/share/$shareFolder
+            echo "Created $home/.local/share/$shareFolder"
+        fi
+        cp -r $scriptDir/$folder/* $home/.local/share/$shareFolder
+        echo "Copied $scriptDir/$folder/* to $home/.local/share/$shareFolder"
+    done
+}
+
+main
